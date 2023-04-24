beaea237f174cf949d597004af89399c794040f0
Francesco <frances01.ferretti@studenti.unicam.it>
2023-04-24 21:57:13 +0200

    zircons

--- src/main/java/net/theferr95/tutorialmod/TutorialMod.java
+++ src/main/java/net/theferr95/tutorialmod/TutorialMod.java
@@ -1,4 +1,4 @@
-package net.theunforgiven95.tutorialmod;
+package net.theferr95.tutorialmod;
 
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
@@ -8,6 +8,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.theferr95.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -18,6 +19,8 @@ public class TutorialMod {
 
     public TutorialMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
