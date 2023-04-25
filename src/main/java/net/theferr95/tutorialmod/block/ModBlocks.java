398c674568f327c20cda12805c6d2bd6889aa9c7
Francesco <frances01.ferretti@studenti.unicam.it>
2023-04-25 02:46:29 +0200

    blocks

--- src/main/java/net/theferr95/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/theferr95/tutorialmod/block/ModBlocks.java
@@ -24,19 +24,14 @@ public class ModBlocks {
             DeferredRegister.create(ForgeRegistries.BLOCKS, TutorialMod.MOD_ID);
 
     public static final RegistryObject<Block> ZIRCON_BLOCK =
-            registerBlock(
-                    "zircon_block",
-                    () -> new Block(
-                            BlockBehaviour.Properties.of(Material.STONE)
-                                    .strength(6f)
-                                    .requiresCorrectToolForDrops()
-                    ),
+            registerBlock("zircon_block",
+                    () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                            .strength(6f)
+                            .requiresCorrectToolForDrops()),
                     ModCreativeModeTab.TUTORIAL_TAB
             );
-
     public static final RegistryObject<Block> ZIRCON_ORE =
-            registerBlock(
-                    "zircon_ore",
+            registerBlock("zircon_ore",
                     () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
                             .strength(6f)
                             .requiresCorrectToolForDrops(),
@@ -45,8 +40,25 @@ public class ModBlocks {
                     ModCreativeModeTab.TUTORIAL_TAB
             );
     public static final RegistryObject<Block> DEEPSLATE_ZIRCON_ORE =
-            registerBlock(
-                    "deepslate_zircon_ore",
+            registerBlock("deepslate_zircon_ore",
+                    () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
+                            .strength(6f)
+                            .requiresCorrectToolForDrops(),
+                            UniformInt.of(3, 7)
+                    ),
+                    ModCreativeModeTab.TUTORIAL_TAB
+            );
+    public static final RegistryObject<Block> ENDSTONE_ZIRCON_ORE =
+            registerBlock("endstone_zircon_ore",
+                    () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
+                            .strength(6f)
+                            .requiresCorrectToolForDrops(),
+                            UniformInt.of(3, 7)
+                    ),
+                    ModCreativeModeTab.TUTORIAL_TAB
+            );
+    public static final RegistryObject<Block> NETHERRACK_ZIRCON_ORE =
+            registerBlock("netherrack_zircon_ore",
                     () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
                             .strength(6f)
                             .requiresCorrectToolForDrops(),
