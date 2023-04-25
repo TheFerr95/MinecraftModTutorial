12a2218d528649962c87b21fcd9983dcf100f698
Francesco <frances01.ferretti@studenti.unicam.it>
2023-04-26 00:12:15 +0200

    eight_ball

--- src/main/java/net/theferr95/tutorialmod/item/ModItems.java
+++ src/main/java/net/theferr95/tutorialmod/item/ModItems.java
@@ -6,6 +6,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.theferr95.tutorialmod.TutorialMod;
+import net.theferr95.tutorialmod.item.custom.EightBallItem;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
@@ -16,6 +17,9 @@ public class ModItems {
 
     public static final RegistryObject<Item> RAW_ZIRCON = ITEMS.register("raw_zircon",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+
+    public static final RegistryObject<Item> EIGHT_BALL = ITEMS.register("eight_ball",
+            () -> new EightBallItem(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB).stacksTo(1)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
